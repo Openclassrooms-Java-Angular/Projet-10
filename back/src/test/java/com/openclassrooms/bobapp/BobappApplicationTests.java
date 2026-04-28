2fa3f2613555a8969a3360eb0ae11418b68b1807
Jocelyn Flament <jocelyn.flament@gmail.com>
2026-04-28 07:32:23 +0200

    add basic back-end test to verify context loading

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -1,13 +1,20 @@
 package com.openclassrooms.bobapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @SpringBootTest
 class BobappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext context;
 
+    @Test
+    void shouldLoadApplicationContext() {
+        assertNotNull("Le contexte Spring ne doit pas être null", context);
+    }
 }
